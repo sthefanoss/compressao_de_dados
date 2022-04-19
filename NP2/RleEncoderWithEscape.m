a913a285e0756db0ad2bc0facb038128892cd8e9
sthefano <sthefanoss@gmail.com>
2022-04-19 19:17:29 -0300

    make rle encoder with escape

--- NP2/RleEncoderWithEscape.m
+++ NP2/RleEncoderWithEscape.m
@@ -1,19 +1,8 @@
 classdef RleEncoderWithEscape
-    %UNTITLED Summary of this class goes here
-    %   Detailed explanation goes here
+
     properties
         escape
     end
-
-   methods (Access = private)
-       function   [Tys,Cys] = splitEscapeTuples(obj, tuples)
-
-       end
-      
-       function   tuples = joinEscapeTuples(obj, Tys,Cys)
-           
-       end
-   end
 
     methods
         function obj = RleEncoderWithEscape(escape)
@@ -25,41 +14,55 @@ classdef RleEncoderWithEscape
                 obj,
                 data (1,:)
             end
-            values(1) = data(1);
-            counts(1) = uint64(1);
-            k = 1;
+            Tys = uint64([1 1]); % {starting index,repetitions}
+            Cys = uint64([1 1]); % {value,repetitions}
+            tIndex = 0;
+            cIndex = 0;
+            if data(1) == obj.escape
+                tIndex = 1;
+                Tys(1,:) = [1,1];
+            else
+                cIndex = 1;
+                Cys(1,:) = [data(1),1];
+            end
+
             for i=2:length(data)
-                if(data(i) == values(k))
-                    counts(k) = counts(k) + 1;
-                else
-                    k = k + 1;
-                    values(k) = data(i);
-                    counts(k) = 1;
+                current = data(i);
+                previuos = data(i-1);
+                if current == previuos && current == obj.escape
+                    Tys(tIndex,2) = Tys(tIndex,2) + 1;
+                elseif current ~= previuos && current == obj.escape
+                    tIndex = tIndex + 1;
+                    Tys(tIndex,:) = [i,1];
+                elseif current == previuos % && current != escape
+                    Cys(cIndex,2) = Cys(cIndex,2) + 1;
+                else %current != previuos && current != escape
+                    cIndex = cIndex+1;
+                    Cys(cIndex,:) = [data(i),1];
                 end
             end
-
-            tuples = cell(1,length(values));
-            for i=1:length(values)
-                tuples{i} = sprintf('%d %d',values(i), counts(i));
-            end
-
-            [Tys,Cys] =  obj.splitEscapeTuples(tuples); 
         end
 
         function data = decode(obj, Tys, Cys)
-             tuples = obj.joinEscapeTuples(Tys,Cys);
-            for i=1:length(tuples)
-                tuple = split(tuples{i},' ');
-                values(i) = str2num(tuple{1});
-                counts(i) = str2num(tuple{2});
-            end
+            data = uint64(1:(sum(Tys(:,2))+sum(Cys(:,2))));
 
-            data = uint64(1:sum(counts));
-            k = 1;
-            for i=1:length(tuples)
-                for j=1:counts(i)
-                    data(k) = values(i);
-                    k = k + 1;
+            tIndex = 1;
+            cIndex = 1;
+            k=1;
+            tuplesLength = length(Tys) + length(Cys);
+            for i=1:tuplesLength
+                if k == Tys(tIndex,1)
+                    for j=1:Tys(tIndex,2)
+                        data(k) = obj.escape;
+                        k = k + 1;
+                    end
+                    tIndex = tIndex + 1;
+                else
+                    for j=1:Cys(cIndex,2)
+                        data(k) = Cys(cIndex,1);
+                        k = k + 1;
+                    end
+                    cIndex = cIndex + 1;
                 end
             end
         end
